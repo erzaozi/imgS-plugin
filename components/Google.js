188a72e8f7de3ac288595d407fa59c424f130210
devil233 <69190444+devil233-ui@users.noreply.github.com>
2025-04-27 05:49:49 +0800

    fix: 禁用浏览器沙盒模式 (#7)
    * Update AnimeTrace.js
    
    api域名变更，见官网说明https://ai.animedb.cn/docs/#/README
    
    * 谷歌搜图调用浏览器沙箱报错
    
    Update Google.js

--- components/Google.js
+++ components/Google.js
@@ -21,6 +21,7 @@ async function Google(url) {
         args: [
             '--disable-blink-features=AutomationControlled',
             '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5410.0 Safari/537.36',
+            '--no-sandbox'
         ],
     };
     if (Config.getConfig().proxy.enable) {
